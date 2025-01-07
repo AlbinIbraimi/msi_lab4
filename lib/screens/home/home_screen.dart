60f5c3badcffc8bdc6936b8cf6c4fa4ab20e14bd
Albin Ibraimi <albinibraimi@outlook.com>
2025-01-07 12:57:20 +0100

    refactor exam create dialog in separate widget

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,113 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:lab_4/models/event.dart';
-import 'package:lab_4/services/storage_service.dart';
+import 'package:lab_4/widgets/exam_dialog.dart';
 import 'package:lab_4/widgets/exams_list.dart';
-import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   void showAddEventDialog(BuildContext context) {
-    final TextEditingController titleController = TextEditingController();
-    final TextEditingController locationController = TextEditingController();
-    final storage = Provider.of<StorageServiceProvider>(context, listen: false);
-    DateTime selectedDate = DateTime.now();
-    TimeOfDay selectedTime = TimeOfDay.now();
-
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Add Event'),
-          content: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextField(
-                  controller: titleController,
-                  decoration: const InputDecoration(labelText: 'Event Title'),
-                ),
-                TextField(
-                  controller: locationController,
-                  decoration:
-                      const InputDecoration(labelText: 'Event Location'),
-                ),
-                const SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Date: ${selectedDate.toLocal()}'),
-                    TextButton(
-                      onPressed: () async {
-                        final DateTime? picked = await showDatePicker(
-                          context: context,
-                          initialDate: selectedDate,
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2101),
-                        );
-                        if (picked != null && picked != selectedDate) {
-                          selectedDate = picked;
-                        }
-                      },
-                      child: const Text('Select Date'),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Time: ${selectedTime.format(context)}'),
-                    TextButton(
-                      onPressed: () async {
-                        final TimeOfDay? picked = await showTimePicker(
-                          context: context,
-                          initialTime: selectedTime,
-                        );
-                        if (picked != null && picked != selectedTime) {
-                          selectedTime = picked;
-                        }
-                      },
-                      child: const Text('Select Time'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context); // Close dialog without saving
-              },
-              child: const Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (titleController.text.isNotEmpty &&
-                    locationController.text.isNotEmpty) {
-                  final newExam = Exam(
-                    title: titleController.text,
-                    dateTime: DateTime(
-                      selectedDate.year,
-                      selectedDate.month,
-                      selectedDate.day,
-                      selectedTime.hour,
-                      selectedTime.minute,
-                    ),
-                    location: locationController.text,
-                  );
-                  storage.createExam(newExam);
-                  Navigator.pop(context); // Close the dialog after saving
-                } else {
-                  // Show error if fields are empty
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please fill in all fields!')),
-                  );
-                }
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        );
+        return const CreateExamDialog();
       },
     );
   }
---
+++ lib/widgets/exam_dialog.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:lab_4/models/event.dart';
+import 'package:lab_4/services/storage_service.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+
+class CreateExamDialog extends StatefulWidget {
+  const CreateExamDialog({super.key});
+
+  @override
+  State<CreateExamDialog> createState() => _CreateExamDialogState();
+}
+
+class _CreateExamDialogState extends State<CreateExamDialog> {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController locationController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  @override
+  Widget build(BuildContext context) {
+    final storage = Provider.of<StorageServiceProvider>(context, listen: false);
+
+    return AlertDialog(
+      title: const Text('Add Exam'),
+      content: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: const InputDecoration(labelText: 'Exam Title'),
+            ),
+            TextField(
+              controller: locationController,
+              decoration: const InputDecoration(labelText: 'Exam Location'),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Date: ${DateFormat('yyyy-MM-dd').format(selectedDate)}'),
+                TextButton(
+                  onPressed: () async {
+                    final DateTime? picked = await showDatePicker(
+                      context: context,
+                      initialDate: selectedDate,
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101),
+                    );
+                    if (picked != null && picked != selectedDate) {
+                      setState(() {
+                        selectedDate = picked;
+                      });
+                    }
+                  },
+                  child: const Text('Select Date'),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Time: ${selectedTime.format(context)}'),
+                TextButton(
+                  onPressed: () async {
+                    final TimeOfDay? picked = await showTimePicker(
+                      context: context,
+                      initialTime: selectedTime,
+                    );
+                    if (picked != null && picked != selectedTime) {
+                      setState(() {
+                        selectedTime = picked;
+                        ;
+                      });
+                    }
+                  },
+                  child: const Text('Select Time'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context); // Close dialog without saving
+          },
+          child: const Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (titleController.text.isNotEmpty &&
+                locationController.text.isNotEmpty) {
+              final newExam = Exam(
+                title: titleController.text,
+                dateTime: DateTime(
+                  selectedDate.year,
+                  selectedDate.month,
+                  selectedDate.day,
+                  selectedTime.hour,
+                  selectedTime.minute,
+                ),
+                location: locationController.text,
+              );
+              storage.createExam(newExam);
+              Navigator.pop(context); // Close the dialog after saving
+            } else {
+              // Show error if fields are empty
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Please fill in all fields!')),
+              );
+            }
+          },
+          child: const Text('Save'),
+        ),
+      ],
+    );
+  }
+}
